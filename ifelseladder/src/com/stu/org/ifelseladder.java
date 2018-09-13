05eaf55bc972a610e4913e12fe1cde2dff7b9b07
fayez kabir <fayezkabir.3@gmail.com>
2018-09-13 21:08:25 +0530

    ifelseladder created

---
+++ ifelseladder/src/com/stu/org/ifelseladder.java
@@ -0,0 +1,63 @@
+package com.stu.org;
+
+public class ifelseladder {
+	public static void main(String args[]) {
+	int z=110;
+	if(z==1)
+	{
+		System.out.println("this is january of the year");
+	}
+	else if(z==2)
+	{
+		System.out.println("this is february of the year");
+		
+	}
+	else if(z==3)
+	{
+		System.out.println("this is march of the year");
+	}
+	else if(z==4)
+	{
+		System.out.println("this is april of the year");
+	}
+	else if(z==5)
+	{
+		System.out.println("this is may of the year");
+	}
+	else if(z==6)
+	{
+		System.out.println("this is june of the year");
+	}
+	else if(z==7)
+	{
+		System.out.println("this is july of the year");
+	}
+	else if(z==8)
+	{
+		System.out.println("this is august of the year");
+	}
+	else if(z==9)
+	{
+		System.out.println("this is september of the year");
+	}
+	else if(z==10)
+	{
+		System.out.println("this is october of the year");
+	}
+	else if(z==11)
+	{
+		System.out.println("this is november of the year");
+	}
+	else if(z==12)
+	{
+		System.out.println("this is december of the year");
+	}
+
+	else 
+	{
+		System.out.println("this fucking month is still not created");
+	}
+
+}
+}
+
