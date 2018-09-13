c96a4283b9e431df007b129fff837334e507565e
fayez kabir <fayezkabir.3@gmail.com>
2018-09-14 01:05:35 +0530

    shitch case added

--- ifelseladder/src/com/stu/org/ifelseladder.java
+++ ifelseladder/src/com/stu/org/ifelseladder.java
@@ -2,53 +2,53 @@ package com.stu.org;
 
 public class ifelseladder {
 	public static void main(String args[]) {
-	int z=110;
-	if(z==1)
+	int month=110;
+	if(month==1)
 	{
 		System.out.println("this is january of the year");
 	}
-	else if(z==2)
+	else if(month==2)
 	{
 		System.out.println("this is february of the year");
 		
 	}
-	else if(z==3)
+	else if(month==3)
 	{
 		System.out.println("this is march of the year");
 	}
-	else if(z==4)
+	else if(month==4)
 	{
 		System.out.println("this is april of the year");
 	}
-	else if(z==5)
+	else if(month==5)
 	{
 		System.out.println("this is may of the year");
 	}
-	else if(z==6)
+	else if(month==6)
 	{
 		System.out.println("this is june of the year");
 	}
-	else if(z==7)
+	else if(month==7)
 	{
 		System.out.println("this is july of the year");
 	}
-	else if(z==8)
+	else if(month==8)
 	{
 		System.out.println("this is august of the year");
 	}
-	else if(z==9)
+	else if(month==9)
 	{
 		System.out.println("this is september of the year");
 	}
-	else if(z==10)
+	else if(month==10)
 	{
 		System.out.println("this is october of the year");
 	}
-	else if(z==11)
+	else if(month==11)
 	{
 		System.out.println("this is november of the year");
 	}
-	else if(z==12)
+	else if(month==12)
 	{
 		System.out.println("this is december of the year");
 	}
