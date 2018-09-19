3595b7c3ddc83d422bcf78b76642cb6d907b995c
fayez kabir <fayezkabir.3@gmail.com>
2018-09-19 11:23:00 +0530

    vowel if else done

---
+++ vowelifelseladder/src/com/stu/pkg/vowelifelseladder.java
@@ -0,0 +1,33 @@
+package com.stu.pkg;
+
+public class vowelifelseladder 
+{
+ public static void main(String agrs[]) 
+  {
+	 char z='p';
+	 if(z=='a') 
+	 {
+		 System.out.println("the letter is vowel");
+	 }
+	 else if(z=='e') 
+	 {
+		 System.out.println("the letter is vowel");
+	 }
+	  else if(z=='i') 
+	 {
+		 System.out.println("the letter is vowel");
+	 }
+	  else if(z=='o') 
+	 {
+		 System.out.println("the letter is vowel");
+	 }
+	  else if(z=='u') 
+	 {
+		 System.out.println("the letter is vowel");
+	 }
+	  else 
+	  {
+		  System.out.println("the letter is not vowel");
+	  }
+  }
+}
