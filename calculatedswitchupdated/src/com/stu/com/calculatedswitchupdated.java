011cabb4750fcc02dfd208e0a21b60c7e7261c75
fayez kabir <fayezkabir.3@gmail.com>
2018-09-19 16:09:14 +0530

    calculator debugging is done

---
+++ calculatedswitchupdated/src/com/stu/com/calculatedswitchupdated.java
@@ -0,0 +1,40 @@
+package com.stu.com;
+
+public class calculatedswitchupdated 
+{
+	{
+		public static void main(String args[]) 
+			{
+			 int a=20;
+			 int b=10;
+			 int z=4;
+			 switch(z)
+			 {
+			 case 1 :
+			 		{
+			 		 System.out.println("ADD="+(a+b));
+			 		 break ;
+			 		}
+			 case 2 :
+				{
+				 System.out.println("SUB="+(a-b));
+				 break ;
+				 }
+			 case 3 :
+				{
+				 System.out.println("MULT="+(a*b));
+				 break ;
+				}
+			 case 4 :
+				{
+				 System.out.println("DIV="+(a/b));
+				 break ;
+				}
+				default :
+				{
+					System.out.println("this is not the number");
+				}
+			}
+		  }
+	
+	}
