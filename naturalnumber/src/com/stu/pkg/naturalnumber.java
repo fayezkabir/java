6fd6b62a8ccc10d3affb8dbd95b1459f91721240
fayez kabir <fayezkabir.3@gmail.com>
2018-09-19 15:24:02 +0530

    natural number done

---
+++ naturalnumber/src/com/stu/pkg/naturalnumber.java
@@ -0,0 +1,19 @@
+package com.stu.pkg;
+
+public class naturalnumber 
+{
+	
+	public static void main(String args[])
+	{
+		int i=0;
+		int z=0;
+		for(i=0;i<=10;i++)
+		{
+			z=z+i;
+		
+		}
+		System.out.println("sum"+z);
+	}
+	
+
+}
