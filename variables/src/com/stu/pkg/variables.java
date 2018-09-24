4ef18fb25a2b410ea95b50731d044dd15ccbbf0d
fayez kabir <fayezkabir.3@gmail.com>
2018-09-25 00:57:27 +0530

    cave of prog variable.prog

---
+++ variables/src/com/stu/pkg/variables.java
@@ -0,0 +1,28 @@
+package com.stu.pkg;
+
+public class variables {
+
+	public static void main(String[] args) 
+	{
+		int myInteger =21;
+		char myChar ='a';
+		float myFloat =2.5f;
+		double myDouble = 5555.3;
+		byte myByte = 24;
+		long myLong = 220;
+		short myShort = 12;
+		boolean myBoolen = true; 
+		System.out.println(myInteger);
+		System.out.println(myChar);
+		System.out.println(myFloat);
+		System.out.println(myDouble);
+		System.out.println(myByte);
+		System.out.println(myLong);
+		System.out.println(myShort);
+		System.out.println(myBoolen);
+		
+	
+
+	}
+
+}
