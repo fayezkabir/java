c605e74b5e315f04c97ea0f9c525ed42642dfacf
fayez kabir <fayezkabir.3@gmail.com>
2018-09-25 00:28:43 +0530

    cave of programming prog 1

---
+++ hlw/src/com/stu/pkg/hlw.java
@@ -0,0 +1,12 @@
+package com.stu.pkg;
+
+public class hlw {
+
+	public static void main(String[] args)
+	{
+		System.out.println("fuck the world");
+System.out.println("fuck u hard sramana");
+
+	}
+
+}
