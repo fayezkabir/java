789a7ebc8a9fc869607dc5516f8d115c1ea38895
fayez kabir <fayezkabir.3@gmail.com>
2018-10-02 12:44:47 +0530

    bolean data type learned clearly

--- Boolean/src/com/stu/pkg/Boolean.java
+++ Boolean/src/com/stu/pkg/Boolean.java
@@ -5,13 +5,28 @@ public class Boolean
 	{
 	  boolean gameOver=true;
 	  int score=100;
-	  int levelComplete=100;
+	  int levelComplete=10;
 	  int bonus=5;
 	  if(gameOver==true) 
 	  {
 		  int finalScore=score*(levelComplete*bonus);
-		  System.out.println("congratulation you have completedthe runs :-"+finalScore);
+		  System.out.println("congratulation you have completedthe runs :- "+finalScore);
 	  }
+	  
+	   gameOver=true;
+	   score=500;
+	   levelComplete=8;
+	   bonus=6;
+	  if(gameOver==true) 
+	  {
+		  int finalScore=score*(levelComplete*bonus);
+		  finalScore+=1000;
+		  
+		  System.out.println("congratulation you have completedthe runs :- "+finalScore);
+	  }
+	  
+	  
+	  
 	}
   
 }
