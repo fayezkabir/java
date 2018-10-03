82d064a5852b170977a5c873465356d353722c44
fayez kabir <fayezkabir.3@gmail.com>
2018-10-04 01:57:48 +0530

    interest rate using for loop done

--- interestUSINGforLoop/src/com/stu/pkg/InterestUSINGforLoop.java
+++ interestUSINGforLoop/src/com/stu/pkg/InterestUSINGforLoop.java
@@ -1,17 +1,16 @@
 package com.stu.pkg;
 
 public class InterestUSINGforLoop {
-	public static void main(String[] args) 
-{
-	for( double interest=2;interest<9;interest++) 
-	{
-          System.out.println("10000 at "+interest+" % interest ="+fayez(interest,10000));
+	public static void main(String[] args) {
+		for (double interest =8; interest >=2; interest--) {
+			System.out.println("10000 at " + interest + " % interest =" + fayez(interest, 10000));
+		}
+
 	}
-	
-}
-public static double fayez(double interest,double  amount) {
-	return(amount*(interest/100));
-	
-}
+
+	public static double fayez(double interest, double amount) {
+		return (amount * (interest / 100));
+
+	}
 
 }
